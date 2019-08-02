fe3883ed7145240845c6f30b6045efe0e62836d4
gabihuizar <gabriellacastaneda@gmail.com>
2019-08-01 21:16:48 -0700

    added jwt to services

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var port = 3000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// We are going to implement a JWT middleware that will ensure the validity of our token. 
+// We'll require each protected route to have a valid access_token sent in the Authorization header
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dev-i1gjsh80.auth0.com/.well-known/jwks.json'
+}),
+  audience: 'http://localhost:4200',
+  issuer: 'https://dev-i1gjsh80.auth0.com/',
+  algorithms: ['RS256']
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(jwtCheck);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// For the private route, we'll add this authCheck middleware
+app.get('/api/loads', jwtCheck, (req,res)=>{
+  let loads = [
+    // Array of loads
+  ];
+  res.json(loads);
+})
+
+app.listen(port);
+console.log('Listening on localhost:3000');
 
 module.exports = app;
